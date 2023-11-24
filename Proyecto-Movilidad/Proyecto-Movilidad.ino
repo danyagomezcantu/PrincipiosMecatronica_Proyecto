b378e0e7ef671a5a27e8754fd1d593734b79ba68
danyagomezcantu <111596636+danyagomezcantu@users.noreply.github.com>
2023-11-24 08:38:20 -0600

    Update Proyecto-Movilidad.ino

--- Proyecto-Movilidad/Proyecto-Movilidad.ino
+++ Proyecto-Movilidad/Proyecto-Movilidad.ino
@@ -1,4 +1,4 @@
-//Conexión Bluetooth a nuestra PC
+// Conexión Bluetooth a nuestra PC
 #include "BluetoothSerial.h"
 
 // Pantalla LCD
@@ -7,117 +7,117 @@
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
 
 BluetoothSerial SerialBT;
-char direccion;
+char direccion;  // Variable para almacenar la dirección recibida por Bluetooth
 
-//Puertos
-#define ENA1 14
-#define InB2 27 //MB
-#define InA2 26 //MA
-#define InB1 25 //MB
-#define InA1 33 //MA
-#define ENA2 32
+// Definición de pines
+#define InA1 33 // Motor A
+#define InB1 25
+#define InA2 26 // Motor B
+#define InB2 27
+#define ENA1 14 // Velocidad motor A
+#define ENA2 32 // Velocidad motor B
 
 void setup() {
   Serial.begin(115200);
-  pinMode(ENA1, OUTPUT);
+  
+  // Configuración de pines como salida
   pinMode(InA1, OUTPUT);
   pinMode(InB1, OUTPUT);
-  pinMode(ENA2, OUTPUT);
   pinMode(InA2, OUTPUT);
   pinMode(InB2, OUTPUT);
+  pinMode(ENA1, OUTPUT);
+  pinMode(ENA2, OUTPUT);
 
-  SerialBT.begin("Robot_Luis_Danya");
-  lcd.init();   // Inicializa el LCD
-  lcd.backlight();   // Enciende la retroiluminación del LCD
-  lcd.setCursor(0,0);   // Establece la posición del cursor en la columna 0, fila 1
+  // Inicialización de la conexión Bluetooth y de la pantalla LCD
+  SerialBT.begin("Robot_Danya_Luis");
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
   lcd.print("Prueba BT Mov");
 }
+
+// Funciones de movimiento
+void adelante() {
+  digitalWrite(InA1, LOW);
+  digitalWrite(InB1, HIGH);
+  digitalWrite(InA2, LOW);
+  digitalWrite(InB2, HIGH);
+  digitalWrite(ENA1, HIGH);
+  digitalWrite(ENA2, HIGH);
+}
+
+void atras() {
+  digitalWrite(InA1, HIGH);
+  digitalWrite(InB1, LOW);
+  digitalWrite(InA2, HIGH);
+  digitalWrite(InB2, LOW);
+  digitalWrite(ENA1, HIGH);
+  digitalWrite(ENA2, HIGH);
+}
+
+void derecha() {
+  digitalWrite(InA1, LOW);
+  digitalWrite(InB1, HIGH);
+  digitalWrite(InA2, LOW);
+  digitalWrite(InB2, LOW);
+  digitalWrite(ENA1, HIGH);
+  digitalWrite(ENA2, HIGH);
+}
+
+void izquierda() {
+  digitalWrite(InA1, LOW);
+  digitalWrite(InB1, LOW);
+  digitalWrite(InA2, LOW);
+  digitalWrite(InB2, HIGH);
+  digitalWrite(ENA1, HIGH);
+  digitalWrite(ENA2, HIGH);
+}
+
+void detenido() {
+  digitalWrite(InA1, LOW);
+  digitalWrite(InB1, LOW);
+  digitalWrite(InA2, LOW);
+  digitalWrite(InB2, LOW);
+  digitalWrite(ENA1, LOW);
+  digitalWrite(ENA2, LOW);
+}
+
 void loop() {
   if (SerialBT.available()) {
     direccion = SerialBT.read();
     Serial.println(direccion);
-    lcd.setCursor(0,0);   
+    lcd.setCursor(0, 0); // Inicializa la posición del texto en la LCD
 
-    if(direccion=='1'){
-      adelante();
-      lcd.clear();
+    if (direccion == '1') { // De acuerdo al valor (caracter) de dirección...
+      adelante(); // Llama a la respectiva función de movimiento
+      lcd.clear(); // Limpia el texto de la LCD en cada cambio de dirección
       lcd.print("1-Adelante");
-      
-    } else if(direccion=='2'){
+
+    } else if (direccion == '2') {
       atras();
       lcd.clear();
-      lcd.print("2-Atras"); 
+      lcd.print("2-Atras");
 
-    } else if(direccion=='3'){
+    } else if (direccion == '3') {
       derecha();
       lcd.clear();
-      lcd.print("3-Derecha"); 
+      lcd.print("3-Derecha");
 
-    } else if(direccion=='4'){
+    } else if (direccion == '4') {
       izquierda();
       lcd.clear();
-      lcd.print("4-Izquierda"); 
+      lcd.print("4-Izquierda");
 
-    } else if (direccion=='5' ) { // Cualquier otra indicación lo vuelve a mover
+    } else if (direccion == '5') { // Cualquier otra dirección lo vuelve a mover
       detenido();
-      lcd.clear(); 
-      lcd.print("5-Detenido"); 
+      lcd.clear();
+      lcd.print("5-Detenido");
     }
     delay(20);
-  } 
-}
-
-void adelante()
-{
-  digitalWrite(ENA1,HIGH);
-  digitalWrite(InA1, LOW);
-  digitalWrite(InB1, HIGH);
-  digitalWrite(ENA2,HIGH);
-  digitalWrite(InA2, LOW);
-  digitalWrite(InB2, HIGH);
-}
-
- void atras()
- {
-   digitalWrite(ENA1,HIGH);
-   digitalWrite(InA1, HIGH);
-   digitalWrite(InB1, LOW);
-   digitalWrite(ENA2,HIGH);
-   digitalWrite(InA2, HIGH);
-   digitalWrite(InB2, LOW);
- }
-
- void derecha(){
-   digitalWrite(ENA1, HIGH);
-   digitalWrite(InA1, LOW);
-   digitalWrite(InB1, HIGH);
-   digitalWrite(ENA2, HIGH);
-   digitalWrite(InA2, LOW);
-   digitalWrite(InB2, LOW);
-}
-
-void izquierda()
-{
-  digitalWrite(ENA1, HIGH);
-  digitalWrite(InA1, LOW);
-  digitalWrite(InB1, LOW);
-  digitalWrite(ENA2, HIGH);
-  digitalWrite(InA2, LOW);
-  digitalWrite(InB2, HIGH);
-}
-
-void detenido()
-{
-  digitalWrite(ENA1, LOW);
-  digitalWrite(InA1, LOW);
-  digitalWrite(InB1, LOW);
-  digitalWrite(ENA2,LOW);
-  digitalWrite(InA2, LOW);
-  digitalWrite(InB2, LOW);
+  }
 }
