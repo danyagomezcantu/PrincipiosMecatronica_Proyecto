aa82d2c957898dec1ea45c3bd84bb6d06d20f459
danyagomezcantu <111596636+danyagomezcantu@users.noreply.github.com>
2023-11-24 18:15:00 -0600

    Update Proyecto-Sensado.ino

--- Proyecto-Sensado/Proyecto-Sensado.ino
+++ Proyecto-Sensado/Proyecto-Sensado.ino
@@ -1,31 +1,34 @@
-// Pantalla LCD
-#include <Wire.h> // Incluye la librería Wire, que permite la comunicación I2C
-#include <LiquidCrystal_I2C.h> // Incluye la librería LiquidCrystal_I2C, que permite controlar pantallas LCD con interfaz I2C
+// Librerías para conectar LCD
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27, 16, 2); // Crea un objeto lcd de la clase LiquidCrystal_I2C, con la dirección I2C 0x27 y especifica que la pantalla tiene 16 columnas y 2 filas
+// Objeto LiquidCrystal_I2C, "lcd", con la dirección I2C predeterminada 0x27, 16 columnas y 2 filas
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-//Para definir la izquierda y la derecha es viendo el robot desde atrás de la pila
-//Puertos
-#define fotoresistorIzq 34 //Fotoresistores
+// Definición de pines y variables
+#define fotoresistorIzq 34 // Fotoresistores
 #define fotoresistorDer 35 
-#define echoPin 12 //Ultrasonico
-#define trigPin 13
-#define infrarojo1 19 //Infrarojos de izquierda a derecha
-#define infrarojo2 18
-#define infrarojo3 5
-#define infrarojo4 17
-#define ENA1 14 //Encoder (2 derecha, 1 izquierda)
-#define InB2 27 //MB
-#define InA2 26 //MA
-#define InB1 25 //MB
-#define InA1 33 //MA
-#define ENA2 32
-#define EncA1 4 // Pin A del encoder 1
-#define EncA2 15 // Pin A del encoder 2
-#define EncB1 2 // Pin B del encoder 1
-#define EncB2 16 // Pin B del encoder 2
 
-//Variables
+#define echoPin 12 // Sensor ultrasónico
+#define trigPin 13
+
+#define infrarrojo1 19 // Sensores infrarrojos de izquierda a derecha
+#define infrarrojo2 18
+#define infrarrojo3 5
+#define infrarrojo4 17
+
+#define ENA1 14 // Motores (A1, A2, B1, B2) y velocidad (ENA1, ENA2)
+#define InB2 27
+#define InA2 26
+#define InB1 25
+#define InA1 33
+#define ENA2 32
+#define EncA1 4 // Pines del encoder
+#define EncA2 15
+#define EncB1 2
+#define EncB2 16
+
+// Variables
 int luzDetectadaIzq;
 int luzDetectadaDer;
 int duracionUltrasonico;
@@ -37,15 +40,19 @@ volatile long pulsesIzq = 0;
 void setup() {
   Serial.begin(115200);
 
-  pinMode(fotoresistorIzq, INPUT); // Configuracion fotoresistencias como entrada
+  // Configuración de pines
+  pinMode(fotoresistorIzq, INPUT); // Configuración de los fotoresistores como entrada
   pinMode(fotoresistorDer, INPUT); 
-  pinMode(trigPin, OUTPUT); // Configuracion del utrasonico
+  
+  pinMode(trigPin, OUTPUT); // Configuración del sensor ultrasónico
   pinMode(echoPin, INPUT);
-  pinMode (infrarojo1, INPUT); // Configuracion de sensores infrarojos
-  pinMode (infrarojo2, INPUT);
-  pinMode (infrarojo3, INPUT);
-  pinMode (infrarojo4, INPUT);
-  pinMode(ENA1, OUTPUT);
+  
+  pinMode(infrarrojo1, INPUT); // Configuración de los sensores infrarrojos
+  pinMode(infrarrojo2, INPUT);
+  pinMode(infrarrojo3, INPUT);
+  pinMode(infrarrojo4, INPUT);
+  
+  pinMode(ENA1, OUTPUT); // Configuración de los motorreductores
   pinMode(InA1, OUTPUT);
   pinMode(InB1, OUTPUT);
   pinMode(ENA2, OUTPUT);
@@ -55,9 +62,11 @@ void setup() {
   pinMode(EncB1, INPUT);
   pinMode(EncA2, INPUT); 
   pinMode(EncB2, INPUT);
+  
   lcd.init();   // Inicializa el LCD
   lcd.backlight();   // Enciende la retroiluminación del LCD
 }
+
 void loop() {
   attachInterrupt(digitalPinToInterrupt(EncA1), Encoder_izquierdo, RISING);
   attachInterrupt(digitalPinToInterrupt(EncA2), Encoder_derecho, RISING);
@@ -69,73 +78,71 @@ void loop() {
   Serial.println();
 }
 
-void monitorSerialPulsos(){
-  Serial.print("Pulsos_Enc_Izq:"+String(pulsesIzq)+" | Pulsos_Enc_Der:"+String(pulsesDer)+" | ");  
+void monitorSerialPulsos() {
+  Serial.print("Pulsos_Enc_Izq:" + String(pulsesIzq) + " | Pulsos_Enc_Der:" + String(pulsesDer) + " | ");  
 }
 
-void Fotoresistor(){
+void Fotoresistor() {
   luzDetectadaIzq = analogRead(fotoresistorIzq);
   luzDetectadaDer = analogRead(fotoresistorDer);
   lcd.setCursor(0,1);
   lcd.print("I:" + String(luzDetectadaIzq) + " | D:" + String(luzDetectadaDer));
   delay(1000);
 
-  Serial.print("LDR_Izq:"+String(luzDetectadaIzq)+" | LDR_Der:"+String(luzDetectadaDer)+" |");
+  Serial.print("LDR_Izq:" + String(luzDetectadaIzq) + " | LDR_Der:" + String(luzDetectadaDer) + " |");
 
   delay(250);
 }
 
-void Ultrasonico(){
+void Ultrasonico() {
   digitalWrite(trigPin, LOW);
-  delayMicroseconds(2); // decía 1000
+  delayMicroseconds(2); 
 
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
 
-  duracionUltrasonico = pulseIn(echoPin,HIGH);
-
+  duracionUltrasonico = pulseIn(echoPin, HIGH);
   distanciaUltrasonico = 0.0343 * duracionUltrasonico * 0.5;
 
   lcd.setCursor(11,0); 
-  lcd.print("L:"+String(distanciaUltrasonico));
-  Serial.print(" Distancia:"+String(distanciaUltrasonico)+" | ");
+  lcd.print("L:" + String(distanciaUltrasonico));
+  Serial.print(" Distancia:" + String(distanciaUltrasonico) + " | ");
 }
 
-void Obstaculos(){
-  //lectura de sensores inflarojos
-  infra1 = digitalRead(infrarojo1);
-  infra2 = digitalRead(infrarojo2);
-  infra3 = digitalRead(infrarojo3);
-  infra4 = digitalRead(infrarojo4);
+void Obstaculos() {
+  // Lectura de sensores infrarrojos
+  infra1 = digitalRead(infrarrojo1);
+  infra2 = digitalRead(infrarrojo2);
+  infra3 = digitalRead(infrarrojo3);
+  infra4 = digitalRead(infrarrojo4);
 
   bool infra[] = {infra1, infra2, infra3, infra4};
   int obstaculo[] = {0, 0, 0, 0};
 
-  for(int i=0;i<4;i++){
-    if(!infra[i]){
+  for(int i = 0; i < 4; i++) {
+    if(!infra[i]) {
       obstaculo[i] = 1;
     }
   }
   
-  Serial.print("Obst:"+String(obstaculo[0])+" "+String(obstaculo[1])+" "+String(obstaculo[2])+" "+String(obstaculo[3])+" | ");
-  lcd.setCursor(0,0);   // Establece la posición del cursor en la columna 0, fila 0
-  lcd.print(String(obstaculo[0])+" "+String(obstaculo[1])+" "+String(obstaculo[2])+" "+String(obstaculo[3]));
+  Serial.print("Obst:" + String(obstaculo[0]) + " " + String(obstaculo[1]) + " " + String(obstaculo[2]) + " " + String(obstaculo[3]) + " | ");
+  lcd.setCursor(0,0);  
+  lcd.print(String(obstaculo[0]) + " " + String(obstaculo[1]) + " " + String(obstaculo[2]) + " " + String(obstaculo[3]));
 }
 
-void Encoder_izquierdo(){
-  if(digitalRead(EncB1)==HIGH){
-      pulsesIzq++;
+void Encoder_izquierdo() {
+  if(digitalRead(EncB1) == HIGH) {
+    pulsesIzq++;
   } else {
     pulsesIzq--;
   }  
 }
 
-void Encoder_derecho(){
-  if(digitalRead(EncB2)==HIGH){
+void Encoder_derecho() {
+  if(digitalRead(EncB2) == HIGH) {
     pulsesDer++;
   } else {
     pulsesDer--;
   }
-
 }
